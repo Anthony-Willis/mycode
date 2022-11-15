38481f1b1453635f8a38571bf95fbd7aef5b0736
anthony <anthonywillis1997@gmail.com>
2022-11-15 21:58:14 +0000

    mixed lists

---
+++ inputpractice.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   CHALLENGE for inputs"""    
+
+def main():
+
+    user_input = input("Please enter your name: ")
+    
+    weekday = input("Please input the day of the week: ")
+    print("Hello, ", user_input,"! Happy ", weekday, sep="")
+main()
+
